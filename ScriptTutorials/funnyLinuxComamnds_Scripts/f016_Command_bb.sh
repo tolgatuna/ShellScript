f12cc1d2650bf00757c008f5cafda1afb9a987f1
okangokalp <okangokalp@outlook.com>
2016-09-02 07:57:55 -0700

    BB COmmand Saved

--- ScriptTutorials/funnyLinuxComamnds_Scripts/f016_Command_bb.sh
+++ ScriptTutorials/funnyLinuxComamnds_Scripts/f016_Command_bb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #	16. Command: bb
-# 	First install “apt-get insatll bb” and then, type “bb” in terminal and see what happens.
+# 	First install “apt-get install bb” and then, type “bb” in terminal and see what happens.
 #    
 #	Install :
-# sudo apt-get insatll bb
+# sudo apt-get install bb
 
 bb
